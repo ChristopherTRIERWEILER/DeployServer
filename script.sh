785e980946ae9a432f19dca193b72373359d9e39
Christopher TRIERWEILER <ctrierweiler@cyber-l.com>
2018-02-28 16:21:53 +0100

    modif condition apache/nginx

--- script.sh
+++ script.sh
@@ -132,25 +132,28 @@ fi
 php7_apache(){
     echo -e "\033[1;32mInstallation de PHP 7.0\e[0m"
     sudo apt-get update
-    sudo apt-get install php-fpm php php-json php-mysql php-mbstring php-mcrypt php-xml php-xmlrpc php-curl php-gd curl git
-    sudo apt-get install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi  php7.0 libapache2-mod-php7.0
-    sudo apt-get install php7.0
+    sudo apt-get install libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-json
+    # Utils apache2
+    sudo apt-get install libapache2-mod-php7.0
+    sudo apt install mysql-server php7.0-mysql
 }
 
 php71_apache(){
     echo -e "\033[1;32mInstallation de PHP 7.1\e[0m"
     sudo apt-get update
-    sudo apt-get install php-fpm php php-json php-mysql php-mbstring php-mcrypt php-xml php-xmlrpc php-curl php-gd curl git
-    sudo apt-get install php7.1-mysql php7.1-curl php7.1-json php7.0-cgi php7.1 libapache2-mod-php7.1
-    sudo apt-get install php7.1
+    sudo apt-get install php7.1 php7.1-xml php7.1-mbstring php7.1-mysql php7.1-json php7.1-curl php7.1-cli php7.1-common php7.1-mcrypt php7.1-gd libapache2-mod-php7.1 php7.1-zip
+    # Utils apache2
+    sudo apt-get install libapache2-mod-php7.1
+    sudo apt install mysql-server php7.1-mysql
 }
 
 php72_apache(){
     echo -e "\033[1;32mInstallation de PHP 7.2\e[0m"
     sudo apt-get update
-    sudo apt-get install php-fpm php php-json php-mysql php-mbstring php-mcrypt php-xml php-xmlrpc php-curl php-gd curl git
-    sudo apt-get install php7.2-mysql php7.2-curl php7.2-json php7.2-cgi php7.2 libapache2-mod-php7.2
-    sudo apt-get install php7.2
+    sudo apt-get install php7.2-curl php7.2-gd php7.2-json php7.2-mbstring php7.2-mcrypt
+    # Utils apache2
+    sudo apt-get install libapache2-mod-php7.2
+    sudo apt install mysql-server php7.2-mysql
 }
 
 # Nginx
@@ -158,27 +161,21 @@ php7_nginx(){
     echo -e "\033[1;32mInstallation de PHP 7.0\e[0m"
     sudo apt-get update
     sudo apt-get --no-install-recommends php7.0 php7.0-fpm php7.0-mysql php7.0-curl php7.0-json php7.0-gd php7.0-mcrypt php7.0-msgpack php7.0-memcached php7.0-intl php7.0-sqlite3 php7.0-gmp php7.0-geoip php7.0-mbstring php7.0-redis php7.0-xml php7.0-zip
-    sudo apt-get install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi php7.0 libapache2-mod-php7.0
-    sudo apt-get install php7.0
 }
 
 php71_nginx(){
     echo -e "\033[1;32mInstallation de PHP 7.1\e[0m"
     sudo apt-get update
-    sudo apt-get install --no-install-recommends php7.1 php7.1-fpm php7.1-mysql php7.1-curl php7.1-json php7.1-gd php7.1-mcrypt php7.1-msgpack php7.1-memcached php7.1-intl php7.1-sqlite3 php7.1-gmp php7.1-geoip php7.1-mbstring php7.1-redis php7.1-xml php7.1-zip
-    sudo apt-get install php7.1-mysql php7.1-curl php7.1-json php7.1-cgi php7.1 libapache2-mod-php7.1
-    sudo apt-get install php7.1
+    sudo apt-get install php7.1-cli php7.1-common php7.1-json php7.1-opcache php7.1-mysql php7.1-mbstring php7.1-mcrypt php7.1-zip php7.1-fpm php7.1-ldap php7.1-tidy php7.1-recode php7.1-curl
 }
 
 php72_nginx(){
     echo -e "\033[1;32mInstallation de PHP 7.2\e[0m"
     sudo apt-get update
-    sudo apt-get --no-install-recommends php7.2 php7.2-fpm php7.2-mysql php7.2-curl php7.2-json php7.2-gd php7.2-mcrypt php7.2-msgpack php7.2-memcached php7.2-intl php7.2-sqlite3 php7.2-gmp php7.2-geoip php7.2-mbstring php7.2-redis php7.2-xml php7.2-zip
-    sudo apt-get install php7.2-mysql php7.2-curl php7.2-json php7.2-cgi php7.2 libapache2-mod-php7.2
-    sudo apt-get install php7.2
+    sudo apt-get php7.2-cli php7.2-common php7.2-json php7.2-opcache php7.2-mysql php7.2-mbstring php7.2-mcrypt php7.2-zip php7.2-fpm php7.2-ldap php7.2-tidy php7.2-recode php7.2-curl
 }
 
-OPTION=$(whiptail --title "Menu" --radiolist \ "Choix des versions de PHP" 15 60 4 \
+OPTION=$(whiptail --title "Menu" --checklist \ "Choix des versions de PHP" 15 60 4 \
         "1" "PHP 7.0" ON \
         "2" "PHP 7.1"  OFF \
         "3" "PHP 7.2" OFF 3>&1 1>&2 2>&3)
